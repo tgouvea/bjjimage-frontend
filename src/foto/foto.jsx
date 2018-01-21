8ef9088fe45ec0de6129fa67080804cb493adbeb
Tiago Gouvêa <tiagoaraujodegouvea@gmail.com>
2018-01-21 10:59:18 -0200

    Componente PageHeader

--- src/foto/foto.jsx
+++ src/foto/foto.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default class Foto extends Component {
 
     render(){
         return (
             <div>
-                <h1> Exemplo compontente foto </h1>
+                <PageHeader name='Fotos' small='Cadastro'></PageHeader>
             </div>
         )
     }
--- src/sobre/sobre.jsx
+++ src/sobre/sobre.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1> Exemplo compontente sobre a empresa </h1>
+        <PageHeader name='Sobre' small='a BJJ Images'></PageHeader>
+
+        <h2>História</h2>
+        <p>Lorem ipsum...</p>
+
+        <h2>Missão</h2>
+        <p>Lorem ipsum...</p>
+
+        <h2>Valores</h2>
+        <p>Lorem ipsum...</p>
+
     </div>
 )
