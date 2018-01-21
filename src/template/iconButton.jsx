e5d46aa7ddf7434267fefb7af5cf433bf8b3e947
Tiago Gouvêa <tiagoaraujodegouvea@gmail.com>
2018-01-21 12:04:15 -0200

    Melhorando componente icone com componente if

--- src/template/iconButton.jsx
+++ src/template/iconButton.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import props from './../main/app';
+import If from './if'
 
-export default props => {
+export default props => (
 
-    if(props.hide){
-        return null
-    } else {
-        return (
-            <button className={'btn btn-' + props.style} onClick={props.onClick}>
-                <i className={'fa fa-' + props.icon}></i>
-            </button>
-        )
-    }
-
-}
+    <If test={!props.hide}>
+        <button className={'btn btn-' + props.style} onClick={props.onClick}>
+            <i className={'fa fa-' + props.icon}></i>
+        </button>
+    </If>    
+)
